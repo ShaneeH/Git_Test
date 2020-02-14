6112f7a5cd3247268005ddc47939fd500b218ef2
Shane Hahesy <shahesy@eshopworld.com>
2020-02-14 11:58:10 +0000

    Add project files.

---
+++ Git_Test/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git_Test
+{
+    class Class2
+    {
+        public void Word() {
+            Console.WriteLine("Im another class");
+        }
+
+    }
+}
---
+++ Git_Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Git_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //GIT Test
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Git Test");
+
+            Class2 x = new Class2();
+            x.Word();
+            Console.ReadLine();
+
+        }
+    }
+}
